281a398367b977d6781fad01ba6444956508509c
Alexander Vinnik <v1nnik.ae@yandex.ru>
2023-03-18 16:02:58 +0700

    refactor: fix checkstyle

--- src/main/java/ru/practicum/shareit/item/dto/CommentResponseDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentResponseDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
@@ -13,5 +14,6 @@ public class CommentResponseDto {
     Long id;
     String text;
     String authorName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     LocalDateTime created;
 }
