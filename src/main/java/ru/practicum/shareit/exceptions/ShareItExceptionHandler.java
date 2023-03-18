c21500dc9be635e3a3dc4ec071ecc1810ed2d677
Alexander Vinnik <v1nnik.ae@yandex.ru>
2023-03-18 15:42:47 +0700

    refactor: fix checkstyle

--- src/main/java/ru/practicum/shareit/exceptions/ShareItExceptionHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ShareItExceptionHandler.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.exceptions;
 
-import lombok.Builder;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
