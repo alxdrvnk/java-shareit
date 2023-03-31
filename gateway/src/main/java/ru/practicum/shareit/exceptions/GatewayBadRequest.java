a0c8b1a694c2caa593a5592dad3f539ffc1d8de9
Alexander Vinnik <v1nnik.ae@yandex.ru>
2023-04-01 00:02:21 +0700

    fix: fix checkstyle

--- gateway/src/main/java/ru/practicum/shareit/exceptions/GatewayBadRequest.java
+++ gateway/src/main/java/ru/practicum/shareit/exceptions/GatewayBadRequest.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exceptions;
 
-public class GatewayBadRequest extends RuntimeException{
+public class GatewayBadRequest extends RuntimeException {
 
     public GatewayBadRequest(String message) {
         super(message);
