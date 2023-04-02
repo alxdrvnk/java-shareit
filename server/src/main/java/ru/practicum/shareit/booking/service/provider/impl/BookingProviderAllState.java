3e05442bbe83d879fbeb8b242e492e370f6b1404
Alexander Vinnik <v1nnik.ae@yandex.ru>
2023-04-02 16:45:06 +0700

    refactor: add sorting by id for get booking by state

--- server/src/main/java/ru/practicum/shareit/booking/service/provider/impl/BookingProviderAllState.java
+++ server/src/main/java/ru/practicum/shareit/booking/service/provider/impl/BookingProviderAllState.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.booking.service.provider.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.booking.service.provider.BookingProvider;
@@ -16,13 +17,13 @@ public class BookingProviderAllState implements BookingProvider {
 
     @Override
     public Collection<Booking> getBookingsOfUser(Long userId, String state, int from, int size) {
-        Pageable pageable = PageRequest.of(from / size, size);
+        Pageable pageable = PageRequest.of(from / size, size, Sort.by("id").ascending());
         return bookingRepository.findAllByBookerIdOrderByStartDesc(userId, pageable);
     }
 
     @Override
     public Collection<Booking> getBookingsOfOwnerItems(Long userId, String state, int from, int size) {
-        Pageable pageable = PageRequest.of(from / size, size);
+        Pageable pageable = PageRequest.of(from / size, size, Sort.by("id").ascending());
         return bookingRepository.findAllByOwnerId(userId, pageable);
     }
 }
--- server/src/main/java/ru/practicum/shareit/booking/service/provider/impl/BookingProviderPastState.java
+++ server/src/main/java/ru/practicum/shareit/booking/service/provider/impl/BookingProviderPastState.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.booking.service.provider.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.booking.service.provider.BookingProvider;
@@ -19,14 +20,14 @@ public class BookingProviderPastState implements BookingProvider {
 
     @Override
     public Collection<Booking> getBookingsOfUser(Long userId, String state, int from, int size) {
-        Pageable pageable = PageRequest.of(from / size, size);
+        Pageable pageable = PageRequest.of(from / size, size, Sort.by("id").ascending());
         return bookingRepository
                 .findAllByBookerIdAndEndBeforeOrderByStartDesc(userId, LocalDateTime.now(clock), pageable);
     }
 
     @Override
     public Collection<Booking> getBookingsOfOwnerItems(Long userId, String state, int from, int size) {
-        Pageable pageable = PageRequest.of(from / size, size);
+        Pageable pageable = PageRequest.of(from / size, size, Sort.by("id").ascending());
         return bookingRepository.findAllByOwnerIdAndPastState(userId, LocalDateTime.now(clock), pageable);
     }
 }
